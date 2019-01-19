c210b8f19e243cbca7db9b600d9b9915dcf820b7
drryner <drryner@icloud.com>
2019-01-19 19:41:46 +0500

    Add cabal project

--- gui/src/Main.hs
+++ gui/src/Main.hs
@@ -1,7 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main where
-
+{-# LANGUAGE RecursiveDo       #-}
 import Reflex.Dom
+import Reflex.Extra
+
+bodyElement :: MonadWidget t m => m ()
+bodyElement = do
+    h2 $ text "Text Input - Read Value on Button Click"
+    ti <- textInput $ def & attributes .~ constDyn ("placeholder" =: "keker")
+    evClick <- button "Click Me, Please"
+    br
+    text "Contents of TextInput on last click: "
+    let evText = tagPromptlyDyn (value ti) evClick
+    dynText =<< holdDyn "" evText
 
 main :: IO ()
-main =  mainWidget $ el "p" $ text "Hello, Haskell and Reflex"
+main = mainWidget bodyElement
